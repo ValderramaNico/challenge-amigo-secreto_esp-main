ab23f21433dc92331833a28954282e0edb6c8a72
spl-NicoValderrama <valderrama.nicolas@outlook.com>
2025-03-17 23:25:04 -0300

    improvements en la App para sortear con 2 amigos o más

--- app.js
+++ app.js
@@ -10,23 +10,15 @@ function agregarAmigo() {
     return;
   }
 
-  // Se hace push() del nombre al array
   amigos.push(nombre);
-
-  // Limpia el campo para ingresar un nuevo amigo
   input.value = "";
-
-  // Actualiza la lista visible en el HTML
   verAmigos();
 }
 
 function verAmigos() {
   const lista = document.getElementById("listaAmigos");
-
-  // Limpiar la lista
   lista.innerHTML = "";
 
-  // Iterar el arreglo de amigos y crea un <li> para cada uno
   for (let i = 0; i < amigos.length; i++) {
     const li = document.createElement("li");
     li.textContent = amigos[i];
@@ -35,19 +27,15 @@ function verAmigos() {
 }
 
 function sortearAmigo() {
-    // Validar que existan amigos ingresados
-    if (amigos.length === 0) {
-      alert("No hay amigos disponibles para sortear aleatoriamente.");
-      return;
-    }
-  
-    // Hace la selección aleatoria usando Math.random() y Math.floor()
+  if (amigos.length === 0) {
+    alert("No hay amigos disponibles para sortear aleatoriamente.");
+  } else if (amigos.length === 1) {
+    alert("Debes agregar al menos 2 amigos para realizar el sorteo.");
+  } else {
     const seleccionAleatoria = Math.floor(Math.random() * amigos.length);
-  
-    // Obtiene el nombre 
     const amigoSeleccionado = amigos[seleccionAleatoria];
-  
-    // Muestra el resultado actualizando el contenido del elemento con id "resultado"
+
     const resultadoElemento = document.getElementById("resultado");
     resultadoElemento.innerHTML = "Tu amigo secreto es: " + amigoSeleccionado;
   }
+}
